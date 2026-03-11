e8ea3d296b21f5787f1802c1061872ec347cda5a
Daniel Borges <danielborges@fesaude.niteroi.rj.gov.br>
2026-03-11 18:12:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { Map } from "./Map";
+import { pubs } from "./pubs";
+import { Map as MapIcon } from "lucide-react";
+import clsx from "clsx";
+
+interface PubItemProps {
+  pubInfo: Pub;
+  setToCenter?: () => void;
+}
+
+type Pub = {
+  id: number;
+  name: string;
+  position: number[];
+  region: string;
+  linkMaps: string;
+};
+
+function PubItem({ pubInfo, setToCenter }: PubItemProps) {
+  return (
+    <div
+      className="w-full flex items-center gap-4 border-b border-amber-600 p-3 drop-shadow-lg"
+      onClick={setToCenter}
+    >
+      <span className="font-bold text-lg text-[#6B4226] text-right">
+        #{pubInfo.id}
+      </span>
+      <div className="flex w-full items-center justify-between">
+        <h2 className="font-semibold text-lg text-[#6B4226]">{pubInfo.name}</h2>
+        <a
+          className="bg-[#F26522] text-gray-100 px-2 py-2 w-24 rounded-md flex items-center justify-center gap-2"
+          href={pubInfo.linkMaps}
+          target="_blank"
+        >
+          <MapIcon size={18} />
+          Maps
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export function App() {
+  const [target, setTarget] = useState<[number, number]>();
+  const [itemToShow, setItemToShow] = useState("Mapa");
+
+  return (
+    <div
+      className={clsx(
+        "w-full bg-[#F5E9DA]",
+        itemToShow === "Mapa" && "h-screen",
+      )}
+    >
+      <header className="flex flex-col items-center justify-center px-2 py-6 gap-4">
+        <div className="p-2 drop-shadow-md flex flex-col items-center gap-4">
+          <div className="flex flex-col items-center text-[#C22026]">
+            <h1 className="text-2xl font-black">
+              🏃‍♀️🎂 <span className="italic font-bold">TATA</span>RATONA 🍺🏃‍♀️
+            </h1>
+            <span>24 anos = 24 bares</span>
+          </div>
+          <span className="text-xl font-semibold text-[#F26522] border-y border-amber-700">
+            <span className="font-black">28.03</span> - 14h
+          </span>
+        </div>
+
+        <div className="flex items-center p-1 rounded-md bg-[#9E6240] gap-1">
+          <button
+            className={clsx(
+              "flex items-center justify-center text-[#F8F2DC] px-3 py-1 rounded-md",
+              "transition-colors duration-200",
+              itemToShow === "Bares"
+                ? "bg-[#DEA47E] text-amber-950"
+                : "cursor-pointer",
+            )}
+            type="button"
+            onClick={() => setItemToShow("Bares")}
+          >
+            🍺 Bares
+          </button>
+          <button
+            className={clsx(
+              "flex items-center justify-center text-[#F8F2DC] px-3 py-1 rounded-md",
+              "transition-colors duration-200",
+              itemToShow === "Mapa"
+                ? "bg-[#DEA47E] text-amber-950"
+                : "cursor-pointer",
+            )}
+            type="button"
+            onClick={() => setItemToShow("Mapa")}
+          >
+            🗺️ Mapa
+          </button>
+        </div>
+      </header>
+      <div className="px-2 rounded-2xl">
+        {itemToShow === "Mapa" && (
+          <div>
+            <Map target={target} />
+          </div>
+        )}
+        {itemToShow === "Bares" && (
+          <div>
+            <div className="px-4 flex flex-col gap-4">
+              {pubs.map((pub) => (
+                <PubItem
+                  pubInfo={pub}
+                  setToCenter={() =>
+                    setTarget([pub.position[0], pub.position[1]])
+                  }
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/Map.tsx
@@ -0,0 +1,54 @@
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
+import { pubs } from "./pubs";
+import beerMug from "./assets/beer-mug.png";
+
+interface MapControllerProps {
+  target?: [number, number] | null;
+}
+
+function MapController({ target }: MapControllerProps) {
+  const map = useMap();
+
+  if (target) {
+    map.flyTo(target, 16, { duration: 0.8 });
+  }
+
+  return null;
+}
+
+export function Map({ target }: MapControllerProps) {
+  const customIcon = L.icon({
+    iconUrl: beerMug,
+    iconSize: [50, 50],
+    iconAnchor: [20, 50],
+    popupAnchor: [0, -50],
+    className: "drop-shadow-2xl",
+  });
+  return (
+    <MapContainer
+      center={[-22.902109839094475, -43.10685698806076]}
+      zoom={15}
+      className="w-full h-[650px]"
+      scrollWheelZoom={false}
+    >
+      <MapController target={target} />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        // url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+      />
+      {pubs.map((pub) => (
+        <Marker
+          key={pub.id}
+          position={[pub.position[0], pub.position[1]]}
+          title={pub.name}
+          icon={customIcon}
+        >
+          <Popup>{pub.name}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
